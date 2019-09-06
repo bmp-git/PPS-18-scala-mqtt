85484d43fec15c25a43612edcd4b539cb641318a
Emanuele Pancisi <emanuele.pancisi@studio.unibo.it>
2019-09-06 13:11:35 +0200

    Test refactor

--- src/test/scala/mqtt/parser/FragmentParserTest.scala
+++ src/test/scala/mqtt/parser/FragmentParserTest.scala
@@ -7,28 +7,36 @@ import mqtt.utils.BitImplicits._
 
 class FragmentParserTest extends FunSuite with Matchers {
   //Qos parser
-  test("A qos parser should parse a qos") {
+  test("A QoS parser SHOULD parse a QoS 0") {
     assert(qos().run(Seq(0, 0)) == List((QoS(0), Seq())))
+  }
+  test("A QoS parser SHOULD parse a QoS 1") {
     assert(qos().run(Seq(0, 1)) == List((QoS(1), Seq())))
+  }
+  test("A QoS parser SHOULD parse a QoS 2") {
     assert(qos().run(Seq(1, 0)) == List((QoS(2), Seq())))
   }
-  test("A qos parser should not parse a qos3 [MQTT-3.1.2-14]") {
+  test("A QoS parser SHOULD NOT parse a QoS 3 [MQTT-3.1.2-14]") {
     assert(qos().run(Seq(1, 1)) == List())
   }
   
   //WillFlags parser
-  test("A WillFlags parser should parse") {
+  test("A WillFlags parser SHOULD parse flags when will_flag=1, will_retain=1, will_QoS=2") {
     assert(willFlags().run(Seq(1, 1, 0, 1)) == List((Option(WillFlags(true, QoS(2))), Seq())))
+  }
+  test("A WillFlags parser SHOULD parse flags when will_flag=1, will_retain=0, will_QoS=0") {
     assert(willFlags().run(Seq(0, 0, 0, 1)) == List((Option(WillFlags(false, QoS(0))), Seq())))
   }
-  test("A WillFlags parser should parse empty flags [no will]") {
+  test("A WillFlags parser SHOULD parse empty flags [will_flag=1, will_retain=0, will_QoS=0]") {
     assert(willFlags().run(Seq(0, 0, 0, 0)) == List((Option.empty, Seq())))
   }
-  test("A WillFlags parser should not parse if will flag is 0 and will retain is 1 [MQTT-3.1.2-15]") {
+  test("A WillFlags parser SHOULD NOT parse if will_flag=0, will_retain=1 [MQTT-3.1.2-15]") {
     assert(willFlags().run(Seq(1, 0, 0, 0)) == List())
   }
-  test("A WillFlags parser should not parse if will flag is 0 and qos is > 0 [MQTT-3.1.2-13]") {
+  test("A WillFlags parser SHOULD NOT parse if will_flag=0, QoS=1 [MQTT-3.1.2-13]") {
     assert(willFlags().run(Seq(0, 0, 1, 0)) == List())
+  }
+  test("A WillFlags parser SHOULD NOT parse if will_flag=0, QoS=2 [MQTT-3.1.2-13]") {
     assert(willFlags().run(Seq(0, 1, 0, 0)) == List())
   }
 }
--- src/test/scala/mqtt/parser/ParserTest.scala
+++ src/test/scala/mqtt/parser/ParserTest.scala
@@ -12,66 +12,76 @@ class ParserTest extends FunSuite with Matchers {
   val byte: Seq[Bit] = Seq(1, 1, 1, 1, 1, 1, 1, 1)
   
   //Item parser
-  test("A item parser should parse a single bit") {
+  test("An item parser SHOULD parse a single zero bit") {
     assert(item().run(Seq(zero)) == List((zero, Seq())))
+  }
+  test("An item parser SHOULD parse a single one bit") {
     assert(item().run(Seq(one)) == List((one, Seq())))
+  }
+  test("An item parser SHOULD parse the first bit of a sequence and return the remaining") {
     assert(item().run(some) == List((some.head, some.tail)))
   }
-  test("A item parser should not parse nothing") {
+  test("A item parser SHOULD NOT parse an empty sequence") {
     assert(item().run(Seq()) == List())
   }
   
   //Bit parser
-  test("A bit parser should parse a single specific bit") {
+  test("A Bit parser SHOULD parse a single specific zero bit") {
     assert(bit(zero).run(Seq(zero)) == List((zero, Seq())))
+  }
+  test("A Bit parser SHOULD parse a single specific one bit") {
     assert(bit(one).run(Seq(one)) == List((one, Seq())))
+  }
+  test("A Bit parser SHOULD parse a single specific zero bit in a sequence and return the remaining") {
     assert(bit(zero).run(some) == List((zero, some.tail)))
   }
-  test("A bit parser should not parse a different bit") {
+  test("A Bit parser SHOULD NOT parse a different bit") {
     assert(bit(zero).run(Seq(one)) == List())
   }
   
   //Zero parser
-  test("A zero parser should parse a zero") {
+  test("A Zero parser SHOULD parse a zero bit in a sequence and return the remaining") {
     assert(Parsers.zero().run(some) == List((zero, some.tail)))
   }
-  test("A zero parser should not parse a one") {
+  test("A Zero parser SHOULD NOT parse a one") {
     assert(Parsers.zero().run(Seq(one)) == List())
   }
   
   //One parser
-  test("A one parser should parse a one") {
+  test("A One parser SHOULD parse a one bit") {
     assert(Parsers.one().run(Seq(one)) == List((one, Seq())))
   }
-  test("A one parser should not parse something starting with 0") {
+  test("A One parser SHOULD NOT parse something starting with a zero bit") {
     assert(Parsers.one().run(some) == List())
   }
   
   //Bits parser
-  test("A bits parser should parse n bits") {
+  test("A Bits parser SHOULD parse exactly n bits") {
     assert(bits(some size).run(some) == List((some, Seq())))
+  }
+  test("A Bits parser SHOULD parse first n bits of a n+1 bit sequence and return the last") {
     assert(bits(some.size - 1).run(some) == List((some.take(some.size - 1), Seq(some.last))))
   }
-  test("A bits parser should not parse more than n bits") {
+  test("A Bits parser SHOULD NOT parse n bits if the sequence is less than n bits") {
     assert(bits(some.size + 1).run(some) == List())
   }
-  test("A bits parser should not parse nothing") {
+  test("A Bits parser SHOULD NOT parse and empty sequence") {
     assert(bits(some size).run(Seq()) == List())
   }
   
   //Bytes parser
-  test("A byte parser should parse bytes") {
+  test("A Byte parser SHOULD parse a byte") {
     assert(bytes(1).run(byte) == List((byte.toBytes, Seq())))
   }
-  test("A byte parser should not parse less then a byte") {
+  test("A Byte parser SHOULD NOT parse a byte if sequence is less then a byte") {
     assert(bytes(1).run(Seq()) == List())
   }
   
   //defaultIfNot parser
-  test("defaultIfNot should not consume input if condition is false") {
-    assert(ifConditionFails(zero, item())(false).run(Seq(0)) == List((zero, Seq(zero))))
+  test("A DefaultIfNot parser SHOULD parse and consume input if condition=true") {
+    assert(ifConditionFails(zero, item())(true).run(Seq(1,1)) == List((one, Seq(one))))
   }
-  test("defaultIfNot should consume input if condition is true") {
-    assert(ifConditionFails(zero, item())(true).run(Seq(0)) == List((zero, Seq())))
+  test("A DefaultIfNot parser SHOULD NOT parse and consume input if condition=false") {
+    assert(ifConditionFails(zero, item())(false).run(Seq(0)) == List((zero, Seq(zero))))
   }
 }
