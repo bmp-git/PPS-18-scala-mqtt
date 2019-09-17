9cb99f71230aaa95a680f974ef67094cdca38e8b
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2019-09-17 13:03:39 +0200

    Implemented encode and decode of variable length integer (2.2.2) with a functional approach

--- src/test/scala/mqtt/utils/VariableLengthIntegerTest.scala
+++ src/test/scala/mqtt/utils/VariableLengthIntegerTest.scala
@@ -4,23 +4,34 @@ import mqtt.utils.BitImplicits._
 import org.scalatest.FunSuite
 
 class VariableLengthIntegerTest extends FunSuite {
-  Map[Int, Seq[Char]](
-    0 -> Seq(0x00),
-    1 -> Seq(0x01),
-    10 -> Seq(0x0A),
-    127 -> Seq(0x7F),
-    128 -> Seq(0x80, 0x01),
-    16383 -> Seq(0xFF, 0x7F),
-    16384 -> Seq(0x80, 0x80, 0x01),
-    2097151 -> Seq(0xFF, 0xFF, 0x7F),
-    2097152 -> Seq(0x80, 0x80, 0x80, 0x01),
-    268435455 -> Seq(0xFF, 0xFF, 0xFF, 0x7F)
+  Seq[(Option[Int], Seq[Char])](
+    Option(0) -> Seq(0x00),
+    Option(1) -> Seq(0x01),
+    Option(10) -> Seq(0x0A),
+    Option(127) -> Seq(0x7F),
+    Option(128) -> Seq(0x80, 0x01),
+    Option(16383) -> Seq(0xFF, 0x7F),
+    Option(16384) -> Seq(0x80, 0x80, 0x01),
+    Option(2097151) -> Seq(0xFF, 0xFF, 0x7F),
+    Option(2097152) -> Seq(0x80, 0x80, 0x80, 0x01),
+    Option(268435455) -> Seq(0xFF, 0xFF, 0xFF, 0x7F),
+    Option.empty -> Seq(),
+    Option.empty -> Seq(0xFF, 0xFF, 0xFF, 0xFF, 0x7F),
   ) foreach {
     case (value, encoded) => {
       val encodedString = encoded.map(_.toByte).toBitsSeq.toBinaryString
-      test(s"$value should be encoded in $encodedString") {
-        val data = VariableLengthInteger.encode(value)
-        assert(encoded.map(_.toByte) == data)
+  
+      if (value.isDefined) {
+        test(s"$value should be encoded in $encodedString") {
+          val data = VariableLengthInteger.encode(value.fold(0)(n => n))
+          assert(encoded.map(_.toByte) == data)
+        }
+      }
+  
+      test(s"$encodedString should be decoded in $value") {
+        val decoded = VariableLengthInteger.decode(encoded.map(_.toByte).toStream)
+        assert(decoded._1 == value)
+        assert(decoded._2.isEmpty)
       }
     }
   }
