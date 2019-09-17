327cd25f2cc1c12a56de849d70b6888fb4212eb5
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2019-09-17 13:06:21 +0200

    Minor refactor in PacketFragments and PacketStructure, fix a bug that occurs when remainingLength fragment is the last

--- src/main/scala/mqtt/builder/PacketStructure.scala
+++ src/main/scala/mqtt/builder/PacketStructure.scala
@@ -5,17 +5,24 @@ import mqtt.utils.Bit
 import mqtt.builder.fragments.CommonPacketFragments.empty
 import mqtt.builder.fragments.PacketFragment
 
+import scala.reflect.ClassTag
+
 /**
  * Provides an abstraction for a MQTT 3.1.1 packet structure.
  *
  * @tparam T the type of the packet for which to specify the structure
  */
-trait PacketStructure[T <: Packet] {
+abstract class  PacketStructure[T <: Packet : ClassTag] {
   def fixedHeader: PacketFragment[T]
   
   def variableHeader: PacketFragment[T] = empty
   
   def payload: PacketFragment[T] = empty
   
-  def build(packet: T): Seq[Bit] = (fixedHeader :: variableHeader :: payload).build(packet)
+  def fragment: PacketFragment[T] = fixedHeader :: variableHeader :: payload
+  
+  def build(input: Packet): Option[Seq[Bit]] = input match {
+    case packet: T => Option(fragment build packet)
+    case _ => Option.empty
+  }
 }
