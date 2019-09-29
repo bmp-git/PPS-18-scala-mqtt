f8c5518e05cecb9643f19dfdb63551f57bf1e037
Lorenzo Mondani <lorenzo.mondani@studio.unibo.it>
2019-09-29 12:39:09 +0200

    Add some BrokerState tests.

--- src/test/scala/mqtt/broker/TestBrokerState.scala
+++ src/test/scala/mqtt/broker/TestBrokerState.scala
@@ -1,7 +1,7 @@
 package mqtt.broker
 
 import mqtt.broker.SampleInstances._
-import mqtt.model.Packet.Connack
+import mqtt.model.Topic
 import org.scalatest.{Assertion, FunSuite}
 
 import scala.concurrent.duration.Duration
@@ -78,7 +78,7 @@ class TestBrokerState extends FunSuite {
     assert(bs2.sessionFromClientID(sample_id_1).isEmpty)
   }
   
-  test("BrokerState can update a field of a session") {
+  test("BrokerState can update a field of a session from ClientID.") {
     val bs1 = bs0.setSession(sample_id_1, sample_session_1)
     val newKeepAlive = Duration(100, "minutes")
     val bs2 = bs1.updateSessionFromClientID(sample_id_1, s => {
@@ -87,24 +87,33 @@ class TestBrokerState extends FunSuite {
     bs2.sessionFromClientID(sample_id_1).fold(fail)(s => assert(s.keepAlive == newKeepAlive))
   }
   
-  test("BrokerState can save a will message") {
+  test("BrokerState can update a field of a session from channel.") {
+    val bs1 = bs0.setSession(sample_id_1, sample_session_1.copy(channel = Some(sample_channel_0)))
+    val newKeepAlive = Duration(100, "minutes")
+    val bs2 = bs1.updateSessionFromChannel(sample_channel_0, s => {
+      s.copy(keepAlive = newKeepAlive)
+    })
+    bs2.sessionFromClientID(sample_id_1).fold(fail)(s => assert(s.keepAlive == newKeepAlive))
+  }
+  
+  test("BrokerState can save a will message.") {
     val bs1 = bs0.setWillMessage(sample_channel_0, sample_application_message_0)
     bs1.wills.get(sample_channel_0).fold(fail)(m => assert(m == sample_application_message_0))
   }
   
-  test("BrokerState can update a will message") {
+  test("BrokerState can update a will message.") {
     val bs1 = bs0.setWillMessage(sample_channel_0, sample_application_message_0)
     val bs2 = bs1.setWillMessage(sample_channel_0, sample_application_message_1)
     bs2.wills.get(sample_channel_0).fold(fail)(m => assert(m == sample_application_message_1))
   }
   
-  test("BrokerState can delete a will message") {
+  test("BrokerState can delete a will message.") {
     val bs1 = bs0.setWillMessage(sample_channel_0, sample_application_message_0)
     val bs2 = bs1.deleteWillMessage(sample_channel_0)
     assert(bs2.wills.isEmpty)
   }
   
-  test("BrokerState can take a pending transmission") {
+  test("BrokerState can take a pending transmission.") {
     val s1 = sample_session_0.copy(channel = Option(sample_channel_0), pendingTransmission = Seq(sample_connack_packet_0))
     val bs1 = bs0.setSession(sample_id_0, s1)
     val (_, packets) = bs1.takeAllPendingTransmission
@@ -130,5 +139,32 @@ class TestBrokerState extends FunSuite {
     val bs1 = bs0.setSession(sample_id_0, s1)
     val (bs2, _) = bs1.takeAllPendingTransmission
     bs2.sessionFromClientID(sample_id_0).fold(fail)(s => assert(s.pendingTransmission.contains(sample_connack_packet_0)))
+  }
+  
+  test("BrokerState can save a retain message.") {
+    val bs1 = bs0.setRetainMessage(new Topic(sample_topic_0), sample_application_message_0)
+    bs1.retains.get(new Topic(sample_topic_0)).fold(fail)(m => assert(m == sample_application_message_0))
+  }
+  
+  test("BrokerState can update a retain message.") {
+    val bs1 = bs0.setRetainMessage(new Topic(sample_topic_0), sample_application_message_0)
+    val bs2 = bs1.setRetainMessage(new Topic(sample_topic_0), sample_application_message_1)
+    bs2.retains.get(new Topic(sample_topic_0)).fold(fail)(m => assert(m == sample_application_message_1))
+  }
+  
+  test("BrokerState can delete a retain message.") {
+    val bs1 = bs0.setRetainMessage(new Topic(sample_topic_0), sample_application_message_0)
+    val bs2 = bs1.clearRetainMessage(new Topic(sample_topic_0))
+    assert(bs2.retains.isEmpty)
+  }
+  
+  test("BrokerState can take closing channels.") {
+    val bs1 = bs0.addClosingChannel(sample_channel_0, Seq(sample_disconnect_packet_0))
+    val bs2 = bs1.addClosingChannel(sample_channel_1, Seq(sample_disconnect_packet_0))
+    
+    val (bs3, closing) = bs2.takeClosing
+    
+    assert(closing.size == 2)
+    assert(bs3.closing.isEmpty)
   }
 }
