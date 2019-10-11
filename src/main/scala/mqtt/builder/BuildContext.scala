b2dd953fd6f6e49a631f5e7a5baf5f51792f03f6
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2019-10-11 10:42:05 +0200

    Minor refactor to packet builder components.

--- src/main/scala/mqtt/builder/BuildContext.scala
+++ src/main/scala/mqtt/builder/BuildContext.scala
@@ -5,10 +5,10 @@ object BuildContext {
   /**
    * The build context.
    *
-   * @param parent if present, represent the parent BuilderPair
+   * @param root if present, represent the root BuilderPair
    * @tparam P the type of the object needed to build
    */
-  case class Context[-P](parent: Option[BuilderPair[P]])
+  case class Context[P](root: Option[BuilderPair[P]])
   
   implicit def defaultBuildContext[P]: Context[P] = Context[P](Option.empty)
 }
